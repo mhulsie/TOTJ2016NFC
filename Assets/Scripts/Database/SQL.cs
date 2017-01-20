bdf9ec3b9feee639f79f307307c1498ecda8eab6
Dominique te Riele <dominiqueteriele@hotmail.com>
2017-01-20 12:51:31 +0100

    SQL commentaar en finishing

--- Assets/Scripts/Database/SQL.cs
+++ Assets/Scripts/Database/SQL.cs
@@ -7,6 +7,7 @@ using System;
 
 /// <summary>
 /// The main class that talks with the database. All queries go trough this class
+/// There can only be one of this class. It can be referenced to from everywhere
 /// </summary>
 public class SQL
 {
@@ -31,56 +32,8 @@ public class SQL
     /// <returns>The webrequest.text. This contains all text of the webpage</returns>
     public string executeQuery(string query)
     {
-        //WWW www = new WWW("http://questionmarkgames.nl/API/queryEncrypt.php?q=" + AES_encrypt(WWW.EscapeURL(query)));
         WWW www = new WWW("http://questionmarkgames.nl/API/query.php?s=abcd&q=" + WWW.EscapeURL(query));
         while (!www.isDone) { }
-        //return AES_decrypt(www.text);
         return www.text;
     }
-
-    public static String AES_decrypt(String Input)
-    {
-        RijndaelManaged aes = new RijndaelManaged();
-        aes.KeySize = 256;
-        aes.BlockSize = 128;
-        aes.Mode = CipherMode.CBC;
-        aes.Padding = PaddingMode.None;
-
-        aes.Key = Convert.FromBase64String("PSVJQRk9QTEpNVU1DWUZCRVFGV1VVT0ZOV1RRU1NaWQ=");
-        aes.IV = Convert.FromBase64String("ZG9kb2lzY29vbHRlc3QxMg==");
-        var decrypt = aes.CreateDecryptor();
-        byte[] xBuff = null;
-        using (var ms = new MemoryStream())
-        {
-            using (var cs = new CryptoStream(ms, decrypt, CryptoStreamMode.Write))
-            {
-                byte[] xXml = Convert.FromBase64String(Input);
-                cs.Write(xXml, 0, xXml.Length);
-            }
-            xBuff = ms.ToArray();
-        }
-        String Output = Encoding.UTF8.GetString(xBuff);
-        return Output;
-    }
-
-    public static string Encrypt(string PlainText)
-    {
-        RijndaelManaged aes = new RijndaelManaged();
-        aes.BlockSize = 128;
-        aes.KeySize = 256;
-
-        aes.Mode = CipherMode.CBC;
-        // aes.Padding = PaddingMode.None;
-
-        aes.Key = Convert.FromBase64String("PSVJQRk9QTEpNVU1DWUZCRVFGV1VVT0ZOV1RRU1NaWQ=");
-        aes.IV = Convert.FromBase64String("ZG9kb2lzY29vbHRlc3QxMg==");
-
-
-        ICryptoTransform encrypto = aes.CreateEncryptor();
-
-        byte[] plainTextByte = ASCIIEncoding.UTF8.GetBytes(PlainText);
-        byte[] CipherText = encrypto.TransformFinalBlock(plainTextByte, 0, plainTextByte.Length);
-        return Convert.ToBase64String(CipherText);
-    }
-
 }
